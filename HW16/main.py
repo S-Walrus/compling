986a4afede7b3dcd3ca78715b421720421ada783
s-walrus <swalrus@yandex.ru>
2019-08-09 21:59:12 +0300

    sync

---
+++ HW16/main.py
@@ -0,0 +1,9 @@
+import vk_api
+import random
+import requests
+import json
+
+token = '072c0a9c629733937426b8aa3cf39e4ad5e72ca800519e280ca82344880a1804ddcd47363391b2107c119'
+
+vk = vk_api.VkApi(token=token)
+
