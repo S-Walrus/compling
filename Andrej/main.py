375fd13f089dee0f19ed67ce808df961223cb6b5
s-walrus <swalrus@yandex.ru>
2019-10-16 21:22:14 +0300

    finished the Andrey project

--- Andrej/main.py
+++ Andrej/main.py
@@ -1,38 +1,70 @@
+from os import listdir
+from os.path import isfile
+from math import log2, sqrt
 from collections import Counter
 import sys
 import re
 from pymystem3 import Mystem
 
+filter_stopwords = True
 
-def join(a, b, size):
+
+with open('stopwords-ru.txt') as f:
+    stopwords = set(f.readlines())
+path_list = ['./data/' + f for f in listdir('./data/')
+             if isfile('./data/' + f)]
+s = ''
+for path in path_list:
+    with open(path) as f:
+        s += f.read() + '\n'
+
+stem = Mystem()
+corpus = stem.lemmatize(s)
+corpus = [item for item in corpus
+          if re.match(r'[а-я]+', item) and
+          (not filter_stopwords or item not in stopwords)]
+size = len(max(corpus, key=len))
+N = len(set(corpus))
+
+
+def join(a, b):
     a, b = min(a, b), max(a, b)
     return a.ljust(size) + b.ljust(size)
 
 
-def split(s, size):
+def split(s):
     a = s[:size].strip()
-    b = s[size:].strup()
+    b = s[size:].strip()
     return (a, b)
 
 
-def f(a, b, size, bigram):
-    return bigram[join(a, b, size)]
+onegram = Counter(corpus)
+bigrams = Counter([join(corpus[i], corpus[i+1])
+                   for i in range(len(corpus)-1)])
 
 
-def g(a, onegram):
+def f(a, b):
+    return bigrams[join(a, b)]
+
+
+def g(a):
     return onegram[a]
 
 
-with open('stopwords-ru.txt') as f:
-    stopwords = set(f.readlines()) | \
-        set('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ') | {'\n'}
-with open(sys.argv[1]) as f:
-    s = f.read()
+def mi(n, c):
+    return log2(f(n, c) * N / g(n) / g(c))
 
-stem = Mystem()
-corpus = stem.lemmatize(s)
-corpus = [item for item in corpus if re.match(r'[а-яa-z]+', item)]
-item_size = len(max(corpus, key=len))
-onegram = Counter(corpus)
-bigrams = Counter([join(corpus[i], corpus[i+1], item_size)
-                   for i in range(len(corpus)-1)])
+
+def tscore(n, c):
+    return (f(n, c) - g(n) * g(c) / N) / sqrt(f(n, c))
+
+
+mi_list = [(p, mi(*split(p))) for p in bigrams.keys()]
+t_list = [(p, tscore(*split(p))) for p in bigrams.keys()]
+mi_list.sort(key=lambda x: x[1])
+t_list.sort(key=lambda x: x[1])
+
+with open('mi_filtered.txt', 'w') as f:
+    f.write('\n'.join(map(str, mi_list)))
+with open('tscore_filtered.txt', 'w') as f:
+    f.write('\n'.join(map(str, t_list)))
